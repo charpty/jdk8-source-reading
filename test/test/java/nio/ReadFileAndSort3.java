f2b33dec6e2a655efafdb54b16340d22c7482894
charpty <charpty@gmail.com>
2018-01-09 00:21:07 +0800

    修改：将读入并分桶时间缩短到4秒以内

--- test/test/java/nio/ReadFileAndSort3.java
+++ test/test/java/nio/ReadFileAndSort3.java
@@ -27,7 +27,7 @@ public class ReadFileAndSort3 {
 	private static final byte[] LINE = "\n".getBytes();
 	private static final byte LF = 10;
 	// 前16位代表所属固定块数组
-	private static final long ARRAY_INDEX_SHIFT = 0xFFFF000000000000L;
+	private static final long ARRAY_INDEX_SHIFT = 0x7FFF000000000000L;
 	// 第17-50位代表开始地址
 	private static final long START_SHIFT = 0x0000FFFFFF000000L;
 	// 后51-64位代表结束地址
@@ -39,21 +39,24 @@ public class ReadFileAndSort3 {
 		// 最长的一行字符串长度,因为256并不大，所以直接使用每一种长度作为一个桶即可
 		int strLen = 256;
 		// 使用一层桶时效率最高
-		LinkedList[] buckets = new LinkedList[1 * strLen];
+		LinkedList[] buckets = new LinkedList[62 * strLen];
 		RandomAccessFile raf = new RandomAccessFile(new File("/tmp/large.file"), "r");
+
+		ExecutorService executor = Executors.newCachedThreadPool();
 
 		FileChannel channel = raf.getChannel();
 		// 一次读4M,2^22
 		// 24 + 24 + 16
-		ByteBuffer buffer = ByteBuffer.allocate(1024 * 1024 * 4);
+		ByteBuffer buffer = ByteBuffer.allocate(1024 * 1024 * 1);
 		int count = 0;
 		long readNanos = 0;
 		long divideNanos = 0;
 		long tmpNanos = 0;
 		// 40960个字符中至多有4096个字符串？在我们的场景中是这样的
-		// 使用一个long的高32位存储字符串起始位置，使用低32位存储结束位置
+		// 使用一个long的中间24位存储字符串起始位置，使用低24位存储结束位置
 		byte[][] mem = new byte[1 << 16][];
 		int mi = 0;
+		// 1000万个字符串
 		long[] indexArr = new long[20_000_000];
 
 		while (true) {
@@ -70,66 +73,67 @@ public class ReadFileAndSort3 {
 			System.arraycopy(array, 0, tmpArr, 0, position);
 			// 自解析，一般来说都是满的
 			// 由于都是字母和数字所以对应就是一个byte就是一个char
+			// TODO 是否可以引入多线程并发分割
 			int p = 0;
 			for (int i = 0; i < position; i++) {
 				if (array[i] == LF) {
-					long index = i;
-					index = index + p << 24 + mi << 48;
-					indexArr[count++] = index;
+					indexArr[count++] = i | (((long) p) << 24) | (((long) mi) << 48);
 					p = i + 1;
 				}
 			}
-			// TODO 睡觉了@23:27 将具体的元素划分到具体桶中
 			tmpNanos = tmpNanos + (System.nanoTime() - afterRead);
+			mem[mi++] = tmpArr;
 			buffer.rewind();
 			if (p < array.length - 1) {
 				buffer.put(array, p, array.length - p);
 			}
-			long beforeDivide = System.nanoTime();
-			// 按照字母分桶
-			for (int i = 0; i < count; i++) {
-				long idx = indexArr[i];
-				int s1 = (int) idx >>> 32;
-				int e1 = (int) (idx & END_SHIFT);
-				int len = s1 - e1;
-				// 先按长度分桶
-				int index = len * 62;
-				// 根据level将元素多级划分到不同桶
-				int offset = 0;
-				byte f = array[s1];
-				if (f <= '9') {
-					// 只有字符和数字
-					offset = f - '0';
-				} else if (f <= 'Z') {
-					offset = f - 'A' + 10;
-				} else {
-					offset = f - 'a' + 36;
-				}
-				index = index + offset;
-				LinkedList<Long> list = buckets[index];
-				if (list == null) {
-					list = new LinkedList();
-					buckets[index] = list;
-				}
-				list.add(idx);
-			}
-			divideNanos = divideNanos + (System.nanoTime() - beforeDivide);
 		}
+		long beforeDivide = System.nanoTime();
+		// 按照字母分桶
+		for (int i = 0; i < count; i++) {
+			long idx = indexArr[i];
+			int m = (int) ((idx & ARRAY_INDEX_SHIFT) >>> 48);
+			int s = (int) ((idx & START_SHIFT) >>> 24);
+			int e = (int) (idx & END_SHIFT);
+			int len = e - s;
+			// 先按长度分桶
+			int index = len * 62;
+			// 根据level将元素多级划分到不同桶
+			int offset = 0;
+			byte f = mem[m][s];
+			if (f <= '9') {
+				// 只有字符和数字
+				offset = f - '0';
+			} else if (f <= 'Z') {
+				offset = f - 'A' + 10;
+			} else {
+				offset = f - 'a' + 36;
+			}
+			index = index + offset;
+			LinkedList<Long> list = buckets[index];
+			if (list == null) {
+				list = new LinkedList();
+				buckets[index] = list;
+			}
+			list.add(idx);
+		}
+		divideNanos = divideNanos + (System.nanoTime() - beforeDivide);
 		long afterDivide = System.nanoTime();
 
-		System.out.println(count);
-		System.out.println(TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - start));
-		System.exit(0);
+		//		System.out.println(count);
+		//		System.out.println("tmpNanos" + TimeUnit.NANOSECONDS.toMillis(tmpNanos));
+		//		System.out.println("divideNanos" + TimeUnit.NANOSECONDS.toMillis(divideNanos));
+		//		System.out.println(TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - start));
+		//		System.exit(0);
 		// 开始排序
 		RandomAccessFile result = new RandomAccessFile(new File("/tmp/result.file"), "rw");
-		FixCharsComparator comparator = new FixCharsComparator(1);
+		FixCharsComparator comparator = new FixCharsComparator(mem);
 		FileChannel outputChannel = result.getChannel();
 
-		ExecutorService executor = Executors.newCachedThreadPool();
 		final Boolean[] flags = new Boolean[buckets.length];
 		// 要尽量保证排在前面的任务先执行
 		for (int i = 0; i < buckets.length; i++) {
-			LinkedList<byte[]> list = buckets[i];
+			LinkedList<Long> list = buckets[i];
 			if (list == null || list.isEmpty()) {
 				continue;
 			}
@@ -142,7 +146,7 @@ public class ReadFileAndSort3 {
 		}
 
 		// 排序的动作可以并行
-		ByteBuffer tmp = ByteBuffer.allocate(40960);
+		ByteBuffer tmp = ByteBuffer.allocate(4096000);
 		long writeNanos = 0;
 		for (int i = 0; i < flags.length; i++) {
 			Boolean flag = flags[i];
@@ -150,14 +154,17 @@ public class ReadFileAndSort3 {
 				while (!flag.booleanValue()) {
 
 				}
-				LinkedList<byte[]> list = buckets[i];
+				LinkedList<Long> list = buckets[i];
 				// 最长256
 				int capacity = list.size() * 256;
 				if (capacity > tmp.capacity()) {
 					tmp = ByteBuffer.allocate(capacity);
 				}
-				for (byte[] s : list) {
-					tmp.put(s).put(LINE);
+				for (long l : list) {
+					int m = (int) ((l & ARRAY_INDEX_SHIFT) >>> 48);
+					int s = (int) ((l & START_SHIFT) >>> 24);
+					int e = (int) (l & END_SHIFT);
+					tmp.put(mem[m], s, e - s).put(LINE);
 				}
 				tmp.flip();
 				long t1 = System.nanoTime();
@@ -180,24 +187,34 @@ public class ReadFileAndSort3 {
 		System.out.println("共耗时" + TimeUnit.NANOSECONDS.toMillis((end - start)));
 	}
 
-	static class FixCharsComparator implements Comparator<byte[]> {
+	static class FixCharsComparator implements Comparator<Long> {
 
-		private final int level;
+		private final byte[][] mem;
 
-		public FixCharsComparator(int level) {
-			this.level = level;
+		public FixCharsComparator(byte[][] mem) {
+			this.mem = mem;
 		}
 
 		@Override
-		public int compare(byte[] chars1, byte[] chars2) {
-			// 不可能为null
-			// 一定是长度相等的
-			// 长level个字符一定是相等的
-			for (int i = level; i < chars1.length; i++) {
-				if (chars1[i] > chars2[i]) {
-					return 1;
-				} else if (chars1[i] < chars2[i]) {
+		public int compare(Long l1, Long l2) {
+			int m1 = (int) ((l1 & ARRAY_INDEX_SHIFT) >>> 48);
+			int s1 = (int) ((l1 & START_SHIFT) >>> 24);
+			int e1 = (int) (l1 & END_SHIFT);
+
+			int m2 = (int) ((l2 & ARRAY_INDEX_SHIFT) >>> 48);
+			int s2 = (int) ((l2 & START_SHIFT) >>> 24);
+
+			byte[] b1 = mem[m1];
+			byte[] b2 = mem[m2];
+
+			// 长度总是相等的
+			while (s1 < e1) {
+				byte bb1 = b1[++s1];
+				byte bb2 = b2[++s2];
+				if (bb1 < bb2) {
 					return -1;
+				} else if (bb1 > bb2) {
+					return 1;
 				}
 			}
 			return 0;
