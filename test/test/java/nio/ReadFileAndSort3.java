bfc487506abcdd1020a5b3155a400271b6fc5f4a
charpty <charpty@gmail.com>
2018-01-14 22:55:33 +0800

    修改:通过直接分桶将时间缩短到5秒以内

--- test/test/java/nio/ReadFileAndSort3.java
+++ test/test/java/nio/ReadFileAndSort3.java
@@ -1,18 +1,22 @@
 package test.java.nio;
 
 import java.io.File;
-import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.PriorityQueue;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.PriorityBlockingQueue;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -20,7 +24,7 @@ import java.util.concurrent.TimeUnit;
  * 读出来之后将其排序输出，按照字符串的自然排序升序
  *
  * 多次测试下来，新生代代大小对性能存在明显影响。
- * 对于1000万文本（800多M），新生代1G比较好:-Xms3100M -Xmx3100M -Xmn1G -XX:+PrintGCDetails
+ * 对于1000万文本（800多M），新生代1G比较好:-Xms14G -Xmx14G -Xmn12G -XX:+PrintGCDetails
  *
  * @author charpty
  * @since 2018/1/1
@@ -46,11 +50,19 @@ public class ReadFileAndSort3 {
 		// 因为题目规定只有字符和数字，所以一共26+26+10=62个桶即可
 		// 最长的一行字符串长度,因为256并不大，所以直接使用每一种长度作为一个桶即可
 		int strLen = 256;
-		// 使用一层桶时效率最高
-		LinkedList[] buckets = new LinkedList[62 * strLen];
-		RandomAccessFile raf = new RandomAccessFile(new File("/tmp/large.file"), "r");
+		ExecutorService es = Executors.newWorkStealingPool();
 
-		ExecutorService es = Executors.newCachedThreadPool();
+		// 40960个字符中至多有4096个字符串？在我们的场景中是这样的
+		// 使用一个long的中间24位存储字符串起始位置，使用低24位存储结束位置
+		byte[][] mem = new byte[1 << 16][];
+		FixCharsComparator comparator = new FixCharsComparator(mem);
+
+		// 使用一层桶时效率最高
+		PriorityBlockingQueue[] buckets = new PriorityBlockingQueue[62 * strLen];
+		for (int i = 0; i < buckets.length; i++) {
+			buckets[i] = new PriorityBlockingQueue(1000, comparator);
+		}
+		RandomAccessFile raf = new RandomAccessFile(new File("/tmp/large.file"), "r");
 
 		FileChannel channel = raf.getChannel();
 		// 一次读4M,2^22
@@ -62,14 +74,13 @@ public class ReadFileAndSort3 {
 		long readNanos = 0;
 		long divideNanos = 0;
 		long tmpNanos = 0;
-		// 40960个字符中至多有4096个字符串？在我们的场景中是这样的
-		// 使用一个long的中间24位存储字符串起始位置，使用低24位存储结束位置
-		byte[][] mem = new byte[1 << 16][];
+
 		int mi = 0;
 		// 1000万个字符串
-		long[][] indexArr = new long[1 << 11][1 << 17];
+		long[][] indexArr = new long[1 << 11][3];
 		final int[] countArr = new int[1 << 10];
-		ConcurrentHashMap<Integer, Long> chm = new ConcurrentHashMap<>(1024);
+		HashMap<Integer, Long> chm = new HashMap<>(1024);
+		HashSet<Integer> nnm = new HashSet<>(1024);
 		boolean last = false;
 		while (true) {
 			long beforeRead = System.nanoTime();
@@ -84,9 +95,9 @@ public class ReadFileAndSort3 {
 				continue;
 			}
 			//readNanos = readNanos + (afterRead - beforeRead);
-			byte[] array = buffer.array();
 			int position = buffer.position();
 
+			byte[] array = buffer.array();
 			byte[] tmpArr = new byte[position + 256];
 			// 一次性拷贝好内存，避免大量重复分配与拷贝
 			System.arraycopy(array, 0, tmpArr, 256, position);
@@ -102,40 +113,75 @@ public class ReadFileAndSort3 {
 			final boolean clast = last;
 			mem[mi++] = tmpArr;
 			es.submit(() -> {
-				// TODO 额～这里是要固定增长个数而不是长度，个数是没法固定的。。。
-				// 每次读4M，使用线程池处理每次读到的数据，每个线程单独操作一个桶，在独立中的桶中计数
-				// 使用一个并发MAP处理各次读取的拆包、粘包问题
-				int cs = 0;
-				final int icmi = (int) cmi;
-				long p = 256;
-				int ci = 256;
-				if (tmpArr[ci] == LF) {
-					ci = 257;
-					p = 257;
-				}
-				for (int i = ci; i < tmpArr.length; i++) {
-					if (tmpArr[i] == LF) {
-						// 前256个用于存放上一个
-						indexArr[icmi][cs++] = i | (p << 24) | (cmi << 48);
-						countArr[icmi]++;
-						p = i + 1;
+				try {
+					// TODO 额～这里是要固定增长个数而不是长度，个数是没法固定的。。。
+					// 每次读4M，使用线程池处理每次读到的数据，每个线程单独操作一个桶，在独立中的桶中计数
+					// 使用一个并发MAP处理各次读取的拆包、粘包问题
+					int cs = 0;
+					final int icmi = (int) cmi;
+					long p = 256;
+					int ci = 256;
+					if (tmpArr[ci] == LF) {
+						ci = 257;
+						p = 257;
+						nnm.add(icmi - 1);
 					}
-				}
-				// 并发下收集粘包
-				if (p < tmpArr.length) {
-					long c = tmpArr.length | (p << 24) | (cmi << 48);
-					if (clast) {
-						indexArr[icmi][cs] = c;
-						countArr[icmi]++;
-					} else {
-						chm.put((int) cmi, c);
+					boolean first = true;
+					for (int i = ci; i < tmpArr.length; i++) {
+						if (tmpArr[i] == LF) {
+							long ld = i | (p << 24) | (cmi << 48);
+							if (!first) {
+								int ip = (int) p;
+								int len = i - ip;
+								// 先按长度分桶
+								int index = len * 62;
+								// 根据level将元素多级划分到不同桶
+								int offset = 0;
+								byte f = tmpArr[ip];
+								if (f <= '9') {
+									// 只有字符和数字
+									offset = f - '0';
+								} else if (f <= 'Z') {
+									offset = f - 'A' + 10;
+								} else {
+									offset = f - 'a' + 36;
+								}
+								index = index + offset;
+								PriorityBlockingQueue<Long> queue = buckets[index];
+								queue.add(ld);
+								countArr[icmi]++;
+							} else {
+								// 前256个用于存放上一个
+								indexArr[icmi][cs++] = ld;
+								countArr[icmi]++;
+								first = false;
+							}
+							p = i + 1;
+						}
 					}
+					// 并发下收集粘包
+					if (p < tmpArr.length) {
+						long c = tmpArr.length | (p << 24) | (cmi << 48);
+						if (clast) {
+							indexArr[icmi][cs] = c;
+							countArr[icmi]++;
+						} else {
+							chm.put((int) cmi, c);
+						}
+					}
+				} catch (Exception ex) {
+					ex.printStackTrace();
+					throw ex;
 				}
 			});
 			buffer.rewind();
 		}
 		es.shutdown();
-		es.awaitTermination(2000, TimeUnit.MILLISECONDS);
+		long c1 = System.nanoTime();
+		es.awaitTermination(8000, TimeUnit.MILLISECONDS);
+		long c2 = System.nanoTime();
+		System.out.println("wodengl:" + TimeUnit.NANOSECONDS.toMillis(c2 - c1));
+		System.out.println("t1:" + TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - start));
 		// 处理粘包
 		for (Map.Entry<Integer, Long> entry : chm.entrySet()) {
 			int kmi = entry.getKey();
@@ -144,20 +190,32 @@ public class ReadFileAndSort3 {
 			int s = (int) ((vmi & START_SHIFT) >>> 24);
 			int e = (int) (vmi & END_SHIFT);
 			int len = e - s;
-			byte[] src = mem[kmi];
-			byte[] dest = mem[kmi + 1];
 			int news = 256 - len;
-			System.arraycopy(src, s, dest, news, len);
-			long l = indexArr[kmi + 1][0];
-			l = (l & START_MASK) | ((long) (news) << 24);
-			indexArr[kmi + 1][0] = l;
+			byte[] dest = mem[kmi + 1];
+			if (indexArr[kmi + 1][0] == 0) {
+				for (int x = 0; x < 3; x++) {
+					if (indexArr[kmi][x] == 0) {
+						indexArr[kmi][x] = vmi;
+						countArr[kmi]++;
+						break;
+					}
+				}
+			} else if (nnm.contains(kmi)) {
+				for (int x = 0; x < 3; x++) {
+					if (indexArr[kmi][x] == 0) {
+						indexArr[kmi][x] = vmi;
+						countArr[kmi]++;
+						break;
+					}
+				}
+			} else {
+				System.arraycopy(mem[kmi], s, dest, news, len);
+				long l = indexArr[kmi + 1][0];
+				l = (l & START_MASK) | (((long) news) << 24);
+				indexArr[kmi + 1][0] = l;
+			}
 		}
 		long beforeDivide = System.nanoTime();
-		int sum = 0;
-		for (int i = 0; i < mi; i++) {
-			sum = sum + countArr[i];
-		}
-		System.out.println("行总数" + sum);
 		// 按照字母分桶
 		for (int i = 0; i < mi; i++) {
 			long[] indexs = indexArr[i];
@@ -185,79 +243,60 @@ public class ReadFileAndSort3 {
 					offset = f - 'a' + 36;
 				}
 				index = index + offset;
-				LinkedList<Long> list = buckets[index];
-				if (list == null) {
-					list = new LinkedList();
-					buckets[index] = list;
-				}
+
+				PriorityBlockingQueue<Long> list = buckets[index];
 				list.add(idx);
 			}
 		}
+		int sum = 0;
+		for (int i = 0; i < buckets.length; i++) {
+			sum = sum + buckets[i].size();
+		}
+		System.out.println("sum=" + sum);
+		int sum2 = 0;
+		for (int i = 0; i < countArr.length; i++) {
+			sum2 = sum2 + countArr[i];
+		}
+		System.out.println("sum2=" + sum);
 		divideNanos = divideNanos + (System.nanoTime() - beforeDivide);
 		long afterDivide = System.nanoTime();
-		es.shutdown();
-
-		LinkedList[] uuu = new LinkedList[1024];
-		System.arraycopy(buckets, 3284, uuu, 0, 1024);
-		System.out.println("total:" + total);
 
 		//		System.out.println(count);
 		//		System.out.println(readNanos);
 		//		System.out.println("tmpNanos" + TimeUnit.NANOSECONDS.toMillis(tmpNanos));
 		//		System.out.println("divideNanos" + TimeUnit.NANOSECONDS.toMillis(divideNanos));
-		//		System.out.println(TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - start));
-		//System.exit(0);
+		System.out.println(TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - start));
+		//		System.exit(0);
 		// 开始排序
 		RandomAccessFile result = new RandomAccessFile(new File("/tmp/result.file"), "rw");
-		FixCharsComparator comparator = new FixCharsComparator(mem);
 		FileChannel outputChannel = result.getChannel();
-		es = Executors.newCachedThreadPool();
-
-		final Boolean[] flags = new Boolean[buckets.length];
-		// 要尽量保证排在前面的任务先执行
-		for (int i = 0; i < buckets.length; i++) {
-			LinkedList<Long> list = buckets[i];
-			if (list == null || list.isEmpty()) {
-				continue;
-			}
-			flags[i] = false;
-			final int x = i;
-			es.submit(() -> {
-				Collections.sort(list, comparator);
-				flags[x] = true;
-			});
-		}
 
 		// 排序的动作可以并行
 		ByteBuffer tmp = ByteBuffer.allocate(4096000);
 		long writeNanos = 0;
-		for (int i = 0; i < flags.length; i++) {
-			Boolean flag = flags[i];
-			if (flag != null) {
-				while (!flag.booleanValue()) {
-
-				}
-				LinkedList<Long> list = buckets[i];
-				// 最长256
-				int capacity = list.size() * 256;
-				if (capacity > tmp.capacity()) {
-					tmp = ByteBuffer.allocate(capacity);
-				}
-				for (long l : list) {
-					int m = (int) ((l & ARRAY_INDEX_SHIFT) >>> 48);
-					int s = (int) ((l & START_SHIFT) >>> 24);
-					int e = (int) (l & END_SHIFT);
-					tmp.put(mem[m], s, e - s).put(LINE);
-				}
-				tmp.flip();
-				long t1 = System.nanoTime();
-				outputChannel.write(tmp);
-				writeNanos = writeNanos + (System.nanoTime() - t1);
-				tmp.rewind();
-				tmp.clear();
+		for (int i = 0; i < buckets.length; i++) {
+			PriorityBlockingQueue<Long> list = buckets[i];
+			if (list.isEmpty()) {
+				continue;
 			}
+			// 最长256
+			int capacity = list.size() * 256;
+			if (capacity > tmp.capacity()) {
+				tmp = ByteBuffer.allocate(capacity);
+			}
+			for (long l : list) {
+				int m = (int) ((l & ARRAY_INDEX_SHIFT) >>> 48);
+				int s = (int) ((l & START_SHIFT) >>> 24);
+				int e = (int) (l & END_SHIFT);
+				tmp.put(mem[m], s, e - s).put(LINE);
+			}
+			tmp.flip();
+			long t1 = System.nanoTime();
+			outputChannel.write(tmp);
+			writeNanos = writeNanos + (System.nanoTime() - t1);
+			tmp.rewind();
+			tmp.clear();
 		}
-		es.shutdown();
 		long end = System.nanoTime();
 		System.out.println("tmp" + TimeUnit.NANOSECONDS.toMillis(tmpNanos));
 		System.out.println("共有" + count + "行");
